b3d0b916e357983aae2a878cf8b2eff1de4d5f17
RicardoFernandes2004 <Ricardo.Fernandes02082004@gmail.com>
2024-11-02 14:56:30 -0300

    Update main.c

--- main.c
+++ main.c
@@ -5,4 +5,16 @@
 int main(int argc, char *argv[]) {
 	printf("Bem vindo ao Supermercado Roludo!");
 	return 0;
+	
+	// lucas:
+	// cadastro de produtos
+	// listar produtos
+	// registro de compras
+	
+	// ricardo:
+	// Calcular total
+	// Geração de receba
+	// Sair
+	
+	
 }
